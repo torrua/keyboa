791844aab110b529f289d64f411cfdfdc0a335f4
torrua <torrua@gmail.com>
2020-05-03 20:32:37 +0700

    UPDATE for 2.2.3

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from distutils.core import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name='keyboa',
   packages=['keyboa'],
-  version='2.2.2',
+  version='2.2.3',
   license='MIT',
   description="Telegram Inline Keyboards Generator",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author='torrua',
   author_email='torrua@gmail.com',
   url='https://github.com/torrua/keyboa',
-  download_url='https://github.com/torrua/keyboa/archive/v2.2.2.tar.gz',
+  download_url='https://github.com/torrua/keyboa/archive/v2.2.3.tar.gz',
   keywords=['Generate', 'Inline', 'Keyboard', 'Telegram'],
   install_requires=[
           'pytelegrambotapi',
