a1fa243174e65eaacf4078c93d4c27c3bc7d4206
torrua <torrua@gmail.com>
2021-04-10 16:05:43 +0700

    Rename module keyboards.py to keyboard.py and tests accordingly

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -8,7 +8,7 @@ import sys
 sys.path.insert(0, "%s/../" % os.path.dirname(os.path.abspath(__file__)))
 
 import pytest
-from keyboa.keyboards import Keyboa
+from keyboa.keyboard import Keyboa
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
